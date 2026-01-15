52a271f2ca6e6344f57298e68df32e2cc8ddc537
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-15 22:24:32 +0530

    Add solution for Surrounded Regions problem
    Implemented a solution to capture surrounded regions in a board.

---
+++ Graphs/0130. Surrounded Regions/0130-Surrounded-Regions.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        queue<pair<int,int>> q;
+
+        // Push all boundary 'O's
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i == 0 || j == 0 || i == n - 1 || j == m - 1) &&
+                    board[i][j] == 'O') {
+                    board[i][j] = '#';
+                    q.push({i, j});
+                }
+            }
+        }
+
+        int delRow[4] = {-1, 1, 0, 0};
+        int delCol[4] = {0, 0, -1, 1};
+
+        // BFS to mark all connected boundary regions
+        while (!q.empty()) {
+            auto [r, c] = q.front();
+            q.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int nr = r + delRow[k];
+                int nc = c + delCol[k];
+
+                if (nr >= 0 && nc >= 0 && nr < n && nc < m &&
+                    board[nr][nc] == 'O') {
+                    board[nr][nc] = '#';
+                    q.push({nr, nc});
+                }
+            }
+        }
+
+        // Flip surrounded regions
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                else if (board[i][j] == '#') board[i][j] = 'O';
+            }
+        }
+    }
+};
