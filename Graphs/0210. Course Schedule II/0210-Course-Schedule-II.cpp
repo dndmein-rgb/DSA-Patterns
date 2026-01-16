53f0680f45b28b04a4ef1e2625d29f7575f3d4c2
Divyanshu Rathore <dnd.mein@gmail.com>
2026-01-16 18:34:13 +0530

    Update 0210-Course-Schedule-II.cpp

--- Graphs/0210. Course Schedule II/0210-Course-Schedule-II.cpp
+++ Graphs/0210. Course Schedule II/0210-Course-Schedule-II.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
-        
-    
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int>indegree(numCourses,0);
-        vector<vector<int>>adj(numCourses);
 
-        for(auto& p:prerequisites){
+        // indegree[i] = number of prerequisites for course i
+        vector<int> indegree(numCourses, 0);
+
+        // adjacency list: b -> a means b must be taken before a
+        vector<vector<int>> adj(numCourses);
+
+        // Build graph and indegree array
+        for (auto& p : prerequisites) {
             adj[p[1]].push_back(p[0]);
             indegree[p[0]]++;
         }
-        queue<int>q;
-        int count=0;
-        for(int i=0;i<numCourses;i++){
-            if(indegree[i]==0)q.push(i);
+
+        queue<int> q;
+
+        // Push all courses with no prerequisites
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
         }
-        vector<int>ans;
-        while(!q.empty()){
-            int node=q.front();
+
+        vector<int> ans;
+
+        // Kahn's Algorithm (BFS Topological Sort)
+        while (!q.empty()) {
+            int node = q.front();
             q.pop();
+
+            // Add course to ordering
             ans.push_back(node);
-            for(int neigh:adj[node]){
+
+            // Reduce indegree of dependent courses
+            for (int neigh : adj[node]) {
                 indegree[neigh]--;
-                if(indegree[neigh]==0)q.push(neigh);
+                if (indegree[neigh] == 0)
+                    q.push(neigh);
             }
         }
-       return numCourses == ans.size() ? ans : vector<int>();
- 
+
+        // If all courses are processed, return order
+        // Otherwise, a cycle exists
+        return numCourses == ans.size() ? ans : vector<int>();
     }
 };
